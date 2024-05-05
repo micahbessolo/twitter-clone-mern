2f9afdbd7956f877c74d558c8d9e843fcb1a66c4
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2024-05-05 09:18:26 -0700

    Signup login signout api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,7 +28,7 @@ export const signup = async (req, res) =>
         {
             return res.status(400).json({ error: "Password must be at least 6 characters long" });
         }
-        // hash password
+
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         
@@ -67,14 +67,53 @@ export const signup = async (req, res) =>
 
 export const login = async (req, res) =>
 {
-    res.json({
-        data: "You hit the login enpoint"
-    });
+    try {
+
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const isPasswordCorrect = user && await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect)
+        {
+            return res.status(400).json({ error: "Invalid Username or Password" });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            fullName: user.fullName,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImg: user.profileImg,
+            coverImg: user.coverImg,
+        });
+
+    } catch (error) {
+        console.error(`Error in login: ${error.message}`);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 export const logout = async (req, res) =>
 {
-    res.json({
-        data: "You hit the logout enpoint"
-    });
-}
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.error(`Error in logout: ${error.message}`);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(`Error in getMe controlloer ${error.message}`);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
-const test = dotenv.config();
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
